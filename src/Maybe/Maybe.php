9b31d9d21a736bb2f23ee895f6e288815e0dc1cb
monsieurluge <monsieurluge@gmail.com>
2020-03-09 17:20:03 +0100

    create the Maybe interface

---
+++ src/Maybe/Maybe.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace monsieurluge\Optional;
+
+use Closure;
+
+interface Maybe
+{
+    /**
+     * Binds the action to the optional value, returning a new optional value.
+     *
+     * @param Closure $action a function as follows: <T> -> Maybe<U>
+     *
+     * @return Maybe a Maybe<U>
+     */
+    public function bind(Closure $action): Maybe;
+
+    /**
+     * Returns either the optional value or the default one.
+     *
+     * @param mixed $default
+     *
+     * @return mixed
+     */
+    public function getOr($default);
+
+    /**
+     * Transforms the optional value.
+     *
+     * @param Closure $transformation a function as follows: <T> -> <U>
+     *
+     * @return Maybe a Maybe<U>
+     */
+    public function map(Closure $transformation): Maybe;
+}
