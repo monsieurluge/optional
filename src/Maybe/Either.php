ef581a0765b76906610e71dea9a82ada54314365
monsieurluge <monsieurluge@gmail.com>
2020-03-10 12:17:30 +0100

    none method implementation

--- src/Maybe/Either.php
+++ src/Maybe/Either.php
@@ -4,9 +4,20 @@ namespace monsieurluge\Optional\Maybe;
 
 use monsieurluge\Optional\Maybe\Just;
 use monsieurluge\Optional\Maybe\Maybe;
+use monsieurluge\Optional\Maybe\Nothing;
 
 class Either
 {
+    /**
+     * Returns a "nothing".
+     *
+     * @return Maybe
+     */
+    public static function none(): Maybe
+    {
+        return new Nothing();
+    }
+
     /**
      * Returns a "some value" object.
      *
