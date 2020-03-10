d48bebfdcf1e12b5e2449815a871e15eb1f2919b
monsieurluge <monsieurluge@gmail.com>
2020-03-10 11:47:02 +0100

    new test: map does nothing

--- tests/Maybe/NothingTest.php
+++ tests/Maybe/NothingTest.php
@@ -21,4 +21,22 @@ final class NothingTest extends TestCase
         // THEN the value is as expected
         $this->assertSame('nothing in here', $value);
     }
+
+    /**
+     * @covers Nothing::getOr
+     * @covers Nothing::map
+     */
+    public function testMapDoesNothing()
+    {
+        // GIVEN nothing
+        $nothing = new Nothing();
+
+        // WHEN a "to uppercase" transformation is mapped
+        $uppercased = $nothing->map(function (string $origin) { return strtoupper($origin); });
+
+        // THEN the origin is still nothing
+        $this->assertSame('nothing in here', $nothing->getOr('nothing in here'));
+        // AND the extracted text is the default one
+        $this->assertSame('default value', $uppercased->getOr('default value'));
+    }
 }
