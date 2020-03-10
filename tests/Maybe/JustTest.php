8f7cd610d4ddbe61b32b3dfcb9ee7c99f97ffc25
monsieurluge <monsieurluge@gmail.com>
2020-03-10 10:50:34 +0100

    new test: map a transformation function

--- tests/Maybe/JustTest.php
+++ tests/Maybe/JustTest.php
@@ -21,4 +21,22 @@ final class JustTest extends TestCase
         // THEN the text is as expected
         $this->assertSame('foo bar', $value);
     }
+
+    /**
+     * @covers Just::getOr
+     * @covers Just::map
+     */
+    public function testCanTransformTheValue()
+    {
+        // GIVEN "just a text"
+        $just = new Just('foo bar');
+
+        // WHEN a "to uppercase" transformation is mapped
+        $uppercased = $just->map(function (string $origin) { return strtoupper($origin); });
+
+        // THEN the origin has not been altered
+        $this->assertSame('foo bar', $just->getOr('nothing in here'));
+        // AND the extracted text is as expected
+        $this->assertSame('FOO BAR', $uppercased->getOr('nothing in here'));
+    }
 }
